32c9af9e8f16f0f60869ad5d04ca82f223fa1b4d
Nathan Mechikoff <107635273+vstghadmin@users.noreply.github.com>
2023-08-17 16:53:19 -0700

    Update Folder_Cleanup.ps1
    Script empties specified folders of all files and sub folders.

--- Powershell/Folder_Cleanup.ps1
+++ Powershell/Folder_Cleanup.ps1
@@ -1,5 +1,9 @@
-#This Script will delete the folder listed
-#if you put \ at the end, it will remove anything inside the folder and not the folder itself
-Remove-Item -Path "$env:userprofile\Downloads\*" -Recurse -Force
+# This Script will delete the folder listed
+# If you put \ at the end, it will remove anything inside the folder and not the folder itself
+
+# For Windows directories, you can list them as follows
 Remove-Item -Path "$env:windir\temp\*" -Recurse -Force
+Remove-Item -Path "$env:userprofile\Downloads\*" -Recurse -Force
+
+# For network locations, just put the full path
 Remove-Item -Path "\\networklocation*" -Recurse -Force
