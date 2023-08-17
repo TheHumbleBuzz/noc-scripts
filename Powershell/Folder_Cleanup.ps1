690eae0507a4fd7debe7e2c66d0b2001265c8916
Nathan Mechikoff <107635273+vstghadmin@users.noreply.github.com>
2023-08-17 16:51:11 -0700

    Update Folder_Cleanup.ps1

--- Powershell/Folder_Cleanup.ps1
+++ Powershell/Folder_Cleanup.ps1
@@ -1,3 +1,5 @@
+#This Script will delete the folder listed
+#if you put \ at the end, it will remove anything inside the folder and not the folder itself
 Remove-Item -Path "$env:userprofile\Downloads\*" -Recurse -Force
 Remove-Item -Path "$env:windir\temp\*" -Recurse -Force
 Remove-Item -Path "\\networklocation*" -Recurse -Force
